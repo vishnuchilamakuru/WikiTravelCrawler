ae84cf854ecce1440d6b12c4e27560db68cd8a0d
Vishnu Chilamakuru <vishnu_chilamakuru@yahoo.com>
2012-07-09 17:24:51 +0530

    added input and output folders to fetch html content from web and stor it locally

--- src/wiki_parser.rb
+++ src/wiki_parser.rb
@@ -5,16 +5,18 @@ require 'nokogiri'
 
 class WikiTravelCrawler
 	
-	attr_accessor :wiki_travel_base_url 
+	attr_accessor :wiki_travel_base_url ,:pwd
 	
 	def initialize
 		@wiki_travel_base_url = "http://wikitravel.org/en/"
+		@pwd = `pwd`
 	end
 	
 	def get_content(wiki_travel_page_title)
-		wiki_travel_full_url = URI.escape(@wiki_travel_base_url + wiki_travel_page_title)
-		wiki_travel_html_content = Net::HTTP.get_response(URI.parse(wiki_travel_full_url))
-		
+		puts "Fetching data from Web......."
+		wiki_travel_full_url = URI.escape(@wiki_travel_base_url + wiki_travel_page_title.strip)
+		wiki_travel_html_content = `wget #{wiki_travel_full_url} -O ../input/#{wiki_travel_page_title}`
+
 		return wiki_travel_html_content	
 	end
 
@@ -22,7 +24,11 @@ class WikiTravelCrawler
 		puts "Enter wiki page title you want to parse : "
 		wiki_title = gets
 		wiki_content = get_content(wiki_title)
-		puts wiki_content
+		wiki_doc = Nokogiri::HTML(wiki_content)
+		output_file = File.open("../output/#{wiki_title}","w") 
+		wiki_doc.xpath('//span[@class="editsection"]').each do | method_span |  
+    			output_file.write  "#{method_span.css('a')[0]['title']}\n"  
+		end  
 	end
 
 end
