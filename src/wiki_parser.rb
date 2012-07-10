d4cbb55f715694806aa164320701aa9c0bc33489
Vishnu Chilamakuru <vishnu_chilamakuru@yahoo.com>
2012-07-10 11:11:01 +0530

    Formatted wikiparser.rb

--- src/wiki_parser.rb
+++ src/wiki_parser.rb
@@ -37,26 +37,26 @@ class WikiTravelCrawler
 		wiki_content = get_content(wiki_title)
 		wiki_doc = Nokogiri::HTML(wiki_content)
 		output_file = File.open("../output/#{wiki_title}","w") 
-    sections_completed = []
+	        sections_completed = []
 		wiki_doc.xpath('//span[@class="mw-headline"]').each do | method_span |
-      wiki_section = method_span.inner_text
-      next_element = method_span.parent.next_element
-      latest_sub_section = wiki_section
-      while (next_element != nil && (next_element.xpath('//h3') != nil || next_element.xpath('//h2') != nil || next_element.xpath('//h4') != nil))
-        if next_element.inner_text.include?("[edit]")
-          latest_sub_section = next_element.inner_text
-        else
-          if next_element.inner_text != nil
-            if sections_completed.include?(latest_sub_section)
-              output_file.write  "#{next_element.inner_text}\n"
-            else
-              output_file.write  "#{latest_sub_section} => #{next_element.inner_text}\n"
-              sections_completed << latest_sub_section
-            end
-          end
-        end
-        next_element = next_element.next_element
-      end
+      			wiki_section = method_span.inner_text
+      			next_element = method_span.parent.next_element
+      			latest_sub_section = wiki_section
+     			while (next_element != nil && (next_element.xpath('//h3') != nil || next_element.xpath('//h2') != nil || next_element.xpath('//h4') != nil))
+        			if next_element.inner_text.include?("[edit]")
+          				latest_sub_section = next_element.inner_text
+       				 else
+          				if next_element.inner_text != nil
+            					if sections_completed.include?(latest_sub_section)
+              						output_file.write  "#{next_element.inner_text}\n"
+            					else
+              						output_file.write  "#{latest_sub_section} => #{next_element.inner_text}\n"
+              						sections_completed << latest_sub_section
+            					end
+          				end
+        			end
+        			next_element = next_element.next_element
+     			 end
 		end
 
 	end
